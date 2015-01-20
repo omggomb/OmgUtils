277c3f6002e6fc5d9997d4661d337a68b8938e24
omggomb <omggomb@gmx.de>
2015-01-20 16:54:48 +0100

    Added initial implementation of settings class

---
+++ ApplicationSettingsClass/Setting.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OmgUtils.ApplicationSettingsManagement
+{
+    /// <summary>
+    /// Represents a single setting inside an application
+    /// </summary>
+    public class Setting
+    {
+        /// <summary>
+        /// The string used to identify the setting
+        /// </summary>
+        public string IdentificationName { get; set; }
+
+        /// <summary>
+        /// A human readable name for the setting
+        /// </summary>
+        public string HumanReadableName { get; set; }
+
+        /// <summary>
+        /// The category string, delimited by dots (".")
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// A readable description of the setting's meaning
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// String determining the type of the setting's value
+        /// </summary>
+        public TypeCode ValueType { get; set; }
+
+        /// <summary>
+        /// The value of the setting stored as a object
+        /// </summary>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// Converts the given type string to a typecode
+        /// </summary>
+        /// <param name="sTypeString">The string describing the type</param>
+        /// <returns>The matching typecode</returns>
+        public static TypeCode GetTypeFromTypeString(string sTypeString)
+        {
+            switch (sTypeString)
+            {
+                case ("bool"):
+                    return TypeCode.Boolean;
+                   
+                case("int"):
+                    return TypeCode.Int32;
+
+                case ("double"):
+                    return TypeCode.Double;
+
+                case ("string"):
+                    return TypeCode.String;
+                    
+                default:
+                    return TypeCode.Empty;
+                    
+            }
+        }
+
+        /// <summary>
+        /// Converts a typecode to a typestring
+        /// </summary>
+        /// <param name="code">The typecode to be converted</param>
+        /// <returns>The matching string</returns>
+        public static string GetTypeStringFromCode(TypeCode code)
+        {
+            switch (code)
+            {
+                case TypeCode.Boolean:
+                    return "bool";
+                    
+               
+                case TypeCode.Double:
+                    return "double";
+                    
+                
+                case TypeCode.Int32:
+                    return "int";
+             
+                case TypeCode.String:
+                    return "string";
+            
+                default:
+                    return "empty";
+                  
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given setting is exactly equal to this one
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Setting)
+            {
+                var o = obj as Setting;
+
+                if (IdentificationName == o.IdentificationName &&
+                    HumanReadableName == o.HumanReadableName &&
+                    ValueType == o.ValueType &&
+                    Value == o.Value &&
+                    Category == o.Category)
+                    return true;
+                   
+            }
+
+            return false;
+        }
+
+     
+
+        /// <summary>
+        /// If the valutypes are the same, returns the value of the type T
+        /// </summary>
+        /// <typeparam name="T">Desired type of the settings value</typeparam>
+        /// <returns></returns>
+        public T GetValue<T>(out bool bSucces)
+        {
+            var t = typeof(T);
+            if (Type.GetTypeCode(t) == ValueType)
+            {
+                bSucces = true;
+                return (T)Value;
+            }
+            else
+            {
+                bSucces = false;
+                return default(T);
+            }
+        }
+
+        public T GetValue<T>()
+        {
+            bool bDummy = false;
+            return GetValue<T>(out bDummy);
+        }
+
+        public void SetValueFromString(string sValueAsString)
+        {
+            if (ValueType == TypeCode.Boolean)
+                Value = Boolean.Parse(sValueAsString);
+            else if (ValueType == TypeCode.Int32)
+                Value = Int32.Parse(sValueAsString);
+            else if (ValueType == TypeCode.Double)
+                Value = Double.Parse(sValueAsString);
+            else if (ValueType == TypeCode.String)
+                Value = sValueAsString;
+        }
+    }
+}
---
+++ ApplicationSettingsClass/SettingsManager.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+using OmgUtils.Logging;
+
+namespace OmgUtils.ApplicationSettingsManagement
+{
+    /// <summary>
+    /// Provides a generic application settings management class.
+    /// XML Structure expected:
+    /// Settings
+    ///     Setting identificationName humanReadableName description category value typeAsString
+    ///
+    /// </summary>
+    public class SettingsManager
+    {
+        /// <summary>
+        /// Holds all the loaded settings
+        /// Settings are stored using their IdentificationName
+        /// </summary>
+        public Dictionary<string, Setting> ApplicationSettings { get; set; }
+
+        /// <summary>
+        /// If a file is loaded, stores the path to that file
+        /// </summary>
+        private string sFilePath;
+
+        /// <summary>
+        /// Used to log messages if set
+        /// </summary>
+        private Logger loggerInstance;
+
+        /// <summary>
+        /// Convenience indicator if logger is present
+        /// </summary>
+        private bool bHasLogger;
+
+        /// <summary>
+        /// If loggerInstance is not null, the manager will use logger to communicate
+        /// errors and warnings, else errors will cause exceptions
+        /// </summary>
+        /// <param name="loggerInstance"></param>
+        public SettingsManager(Logger loggerInstance)
+        {
+            sFilePath = "";
+            this.loggerInstance = loggerInstance;
+            bHasLogger = (loggerInstance == null) ? false : true;
+
+            ApplicationSettings = new Dictionary<string, Setting>();
+        }
+
+        /// <summary>
+        /// Loads all settings from the specified xml file
+        /// </summary>
+        /// <param name="sPathToFile"></param>
+        public bool LoadSettingsFromFile(string sPathToFile)
+        {
+            ApplicationSettings.Clear();
+            XmlReader reader = null;
+            try
+            {
+                reader = XmlReader.Create(sPathToFile);
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            {
+                                if (reader.Name == "Setting")
+                                {
+                                    // XML Structure expected:
+                                    // Settings
+                                    //     Setting identificationName humanReadableName description category value typeAsString
+
+                                    var set = new Setting();
+
+                                    set.IdentificationName = reader.GetAttribute("identificationName");
+                                    set.HumanReadableName = reader.GetAttribute("humanReadableName");
+                                    set.Description = reader.GetAttribute("description");
+                                    set.Category = reader.GetAttribute("category");
+
+                                    string typeAsString = reader.GetAttribute("typeAsString");
+                                    set.ValueType = Setting.GetTypeFromTypeString(typeAsString);
+
+                                    string valueAsString = reader.GetAttribute("value");
+                                    set.SetValueFromString(valueAsString);
+
+                                    ApplicationSettings.Add(set.IdentificationName, set);
+                                }
+                            } break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (reader != null)
+                    reader.Close();
+                if (bHasLogger)
+                {
+                    loggerInstance.LogError("[SettingsManager:107] " + e.Message);
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+
+            this.sFilePath = sPathToFile;
+            return true;
+        }
+
+        /// <summary>
+        /// Saves the settings to the same file they have been loaded from
+        /// </summary>
+        /// <returns></returns>
+        public bool SaveSettings()
+        {
+            return SaveSettings(sFilePath);
+        }
+
+        /// <summary>
+        /// Save settings to specified file, override if file already exists
+        /// </summary>
+        /// <param name="sPathToFile"></param>
+        /// <param name="bOverride"></param>
+        /// <returns></returns>
+        public bool SaveSettings(string sPathToFile, bool bOverride = true)
+        {
+            if (File.Exists(sPathToFile) && bOverride)
+                File.Delete(sPathToFile);
+            else if (File.Exists(sPathToFile) && !bOverride)
+            {
+                if (bHasLogger)
+                {
+                    loggerInstance.LogError("[SettingsManager:142] Tried saving settings, but file exists and bOverride is false");
+                    return false;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Tried saving settings, but file exists and bOverride is false");
+                }
+            }
+
+            XmlWriter writer = null;
+
+            try
+            {
+                writer = XmlWriter.Create(sPathToFile, new XmlWriterSettings() { Indent = true, NewLineOnAttributes = true });
+
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Settings");
+
+                foreach (var set in ApplicationSettings.Values)
+                {
+                    // XML Structure expected:
+                    // Settings
+                    //     Setting identificationName humanReadableName description category value typeAsString
+                    writer.WriteStartElement("Setting");
+                    writer.WriteAttributeString("identificationName", set.IdentificationName);
+                    writer.WriteAttributeString("humanReadableName", set.HumanReadableName);
+              
+                    writer.WriteAttributeString("description", set.Description);
+                    writer.WriteAttributeString("category", set.Category);
+
+                    writer.WriteAttributeString("value", (set.Value != null) ? set.Value.ToString() : "null");
+                    writer.WriteAttributeString("typeAsString", Setting.GetTypeStringFromCode(set.ValueType));
+
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                if (writer != null)
+                    writer.Close();
+
+                if (bHasLogger)
+                {
+                    loggerInstance.LogError("[SettingsManager:189] " + e.Message);
+                    return false;
+                }
+                else
+                    throw;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Tries to get the wanted setting, returns null if not found
+        /// </summary>
+        /// <param name="sIdentName"></param>
+        /// <returns></returns>
+        public Setting GetValue(string sIdentName)
+        {
+            Setting output = null;
+
+            if (ApplicationSettings.TryGetValue(sIdentName, out output))
+                return output;
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Sets the given value, or creates it if not present
+        /// </summary>
+        /// <param name="setting"></param>
+        /// <param name="bCreate"></param>
+        /// <returns></returns>
+        public bool SetValue(Setting setting, bool bCreate = true)
+        {
+            return true;
+        }
+
+    }
+}
